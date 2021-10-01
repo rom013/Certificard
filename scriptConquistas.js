a05afeafc508fd6c27aaf6338342f1c7f1ec46d5
Rômullo <81822722+rom013@users.noreply.github.com>
2021-09-30 23:05:29 -0300

    5 commit
    Add media query
    Organização dos arquivos css
    Mudei a posição das redes sociais

---
+++ scriptConquistas.js
@@ -0,0 +1,6 @@
+var openAchievemantCal = setTimeout(function() {
+    document.getElementById("alertAchievemantCal").style.display = `block`
+  }, 5000)
+var closeAchievemantCal = setTimeout(function() {
+    document.getElementById("alertAchievemantCal").style.display = `none`
+  }, 10000)
